7cef3632bc0149f1aba5fcee330fe08f97ac07d1
Ale <eze.giorgi@gmail.com>
2020-03-06 20:16:38 -0300

    Agrega botón para saltar

---
+++ Assets/Scripts/UI/Controller/MobileInputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using experimentomobile.player;
+
+namespace experimentomobile.ui.controller
+{
+    public class MobileInputController : MonoBehaviour
+    {
+        private Player player;
+        [SerializeField]
+        private Button jumpButton;
+
+        private void Start()
+        {
+            player = GameObject.Find("Player").GetComponent<Player>();
+
+            jumpButton.onClick.AddListener(() => player.PrepareJump());
+        }
+    }
+}
