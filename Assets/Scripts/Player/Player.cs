fbcc37ac52e7da8aa886b01cfd76bf63bb1bf720
Ale <eze.giorgi@gmail.com>
2020-03-05 23:44:01 -0300

    Salto del jugador

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,30 +6,38 @@ namespace experimentomobile.player
     public class Player : MonoBehaviour
     {
         private Rigidbody playerRigidbody;
+        private Animator playerAnimator;
 
         [SerializeField]
-        private float jumpForce = 300f;
+        private float jumpForce = 200f;
         [SerializeField]
         private bool isOnGround;
 
         private void Start()
         {
             playerRigidbody = GetComponent<Rigidbody>();
+            playerAnimator = GetComponent<Animator>();
             isOnGround = true;
         }
 
         private void FixedUpdate()
-        {
+        {            
             if (isOnGround && Input.GetKey(KeyCode.Space))
             {
-                Jump();
+                PrepareJump();                
             }
+        }
+
+        private void PrepareJump()
+        {
+            isOnGround = false;
+            playerAnimator.SetTrigger("Jump_trig");
         }
 
         private void Jump()
         {
-            isOnGround = false;
-            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);            
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -37,6 +45,7 @@ namespace experimentomobile.player
             if (collision.gameObject.CompareTag(Tags.BASE))
             {
                 isOnGround = true;
+                playerAnimator.SetBool("OnGround_b", true);
             }
         }
     }
